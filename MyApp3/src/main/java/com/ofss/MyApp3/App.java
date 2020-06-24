2427fc1e413e5b7daf514831b8ee426f7bae9315
Vijayhv <vijayhv@gmail.com>
2020-06-24 11:50:05 +0530

    first change

---
+++ MyApp3/src/main/java/com/ofss/MyApp3/App.java
@@ -0,0 +1,15 @@
+package com.ofss.MyApp3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("Value is "+Calculator.calc(4, 5));
+    }
+}
---
+++ MyApp3/src/main/java/com/ofss/MyApp3/Calculator.java
@@ -0,0 +1,10 @@
+package com.ofss.MyApp3;
+
+public class Calculator {
+	
+	public static int calc(int a, int b) {
+		
+		return (a*b);
+	}
+
+}
